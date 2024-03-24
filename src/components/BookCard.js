5306de907600c97aa282a577a11ca7a73b148661
ezatjr <ezatj@hiof.no>
2024-03-24 20:00:36 +0100

    la in kommentar på Booklist.js

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -16,6 +16,6 @@ const BookCard = ({ book, averageRating }) => {
       </a>
     </div>
   );
-};a
+};
 
 export default BookCard;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import BookCard from './BookCard'; 
-import '../styles/BookCard.scss';
-import '../styles/BookList.scss';
+import BookCard from './BookCard'; // Importerer BookCard-komponenten fra './BookCard'
+import '../styles/BookCard.scss'; // Importerer stilene for BookCard-komponenten
+import '../styles/BookList.scss'; // Importerer stilene for BookList-komponenten
 
+
+//Lager en funksjonell komponent kalt BookList som tar en prop books.
 const BookList = ({ books }) => {
+  //Returner JSX som representerer strukturen til Booklist-Komponenten.
   return (
     <div className="book-list-container">
-      {books.map((book, index) => (
-        <div key={index} className="book-card-wrapper">
-          <BookCard book={book}/> 
+      {/* Går gjennom hver bok i 'books'-arrayen og rendrer en BookCard-komponent for hver */}
+      {books.map((book, index) => (  
+        <div key={index} className="book-card-wrapper"> {/* En div for hvert BookCard med et unikt 'key' basert på indeks */}
+          <BookCard book={book}/>  {/* Rendrer BookCard-komponenten for den gjeldende boken og sender bokobjektet som en prop */}
         </div>
       ))}
     </div>
